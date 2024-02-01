d513af98919d594aef36556cb532a72347149f81
unknown <mlason@goldencomm.com>
2024-02-02 00:58:08 +0100

    feat: add shades to radar chart

--- src/components/WeatherChart.tsx
+++ src/components/WeatherChart.tsx
@@ -37,11 +37,27 @@ export const WeatherChart = ({ weather }: { weather: Weather }) => {
   const options: ChartOptions = {
     scales: {
       r: {
-        // backgroundColor: 'lightgray',
+        backgroundColor: 'white',
         suggestedMin: 0,
         suggestedMax: 10,
         grid: {
-          color: 'darkgray',
+          color: [
+            'rgba(200,200,200,0.7)',
+            'rgba(150,150,150,0.4)',
+            'rgba(50,50,50,0.3)',
+            'rgba(154, 200, 235, 0.5)',
+            'rgba(100,50,50,0.15)',
+            'rgba(150,200,150,0.2)',
+            'rgba(150,200,150,0.3)',
+            'rgba(255,100,100,0.2)',
+            'rgba(255,100,100,0.1)',
+            'rgba(250, 150, 90, 0.1)',
+            'rgba(250, 150, 90, 0.15)',
+          ],
+          lineWidth: 10,
+        },
+        ticks: {
+          display: false,
         },
       },
     },
